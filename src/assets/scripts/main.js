321ff7558949ee42a65aa204c90d63db3b9df9e7
Chester Kuo <docilas555@gmail.com>
2017-11-12 22:05:15 +0800

    Add tour page

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,1 +1,20 @@
-var a = 0;
+$(function() {
+
+
+
+
+	// popup close
+	function popClose(tar){
+		tar.removeClass('show')
+	}
+
+	$('.popup .btn-close').on('click', function(){
+		var page = $(this).parents('.popup');
+		console.log(page)
+		if ( page.hasClass('show') ){
+			popClose(page);
+		}
+
+	});
+
+});
