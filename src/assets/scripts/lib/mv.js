12bf420ce9ca8bd56a987bca7c0b983123fa86ec
MasterKuei <hank5678tw@gmail.com>
2017-11-23 14:35:31 +0800

    mv.js update

--- src/assets/scripts/lib/mv.js
+++ src/assets/scripts/lib/mv.js
@@ -62,7 +62,7 @@ var mv = function() {
 			var h = 400;
 			var app = new PIXI.Application(w, h, {forceCanvas: false, view: document.getElementById('firework'), transparent:true});
 
-			var texture = PIXI.Texture.fromImage('/tc/assets/images/test/particle2.png');
+			var texture = PIXI.Texture.fromImage('/tc/assets/images/mv/particle.png');
 
 			var container = new PIXI.particles.ParticleContainer(10000, {
 			    scale: false,
@@ -270,7 +270,7 @@ var mv = function() {
 				var mask = new Image();
 				maskArray.push(mask);
 				mask.onload = imgLoaded;
-				mask.src = "/tc/assets/images/test/mask"+(i+1)+"-mobile.png";
+				mask.src = "/tc/assets/images/mv/mask"+(i+1)+"-mobile.png";
 			}
 
 		}
@@ -746,7 +746,7 @@ var mv = function() {
 			completeFn = complete;
 			loadedCounter = 0;
 			var req = new XMLHttpRequest();
-			req.open('GET', '/tc/assets/videos/mv2.mp4', true);
+			req.open('GET', '/tc/assets/videos/mv.mp4', true);
 			req.responseType = 'blob';
 			req.onload = function() {
 			   if (this.status === 200) {
@@ -798,10 +798,8 @@ var mv = function() {
 				requestAnimationFrame(getCurrentVideoFrame);
 			    var curTime = v.currentTime;
 			    var theCurrentFrame = Math.floor(curTime*frameRate);
-			    console.log( theCurrentFrame );
 			    if( theCurrentFrame == lastCurrentFrame ) return;
 			    lastCurrentFrame = theCurrentFrame;
-			    // console.log( theCurrentFrame );
 			    if( f.loadComplete ) {
 				    if( theCurrentFrame >= 2846 && theCurrentFrame <= 2993 && !f.isShooting) {
 						f.show();
